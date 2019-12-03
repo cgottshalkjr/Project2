75c9ccc427d6f1e4c7986d75eec53dc581f4c546
Leah Kurtz <lfkurtz7@gmail.com>
2019-12-03 14:15:01 -0500

    schema file created

--- models/schema.sql
+++ models/schema.sql
@@ -1,8 +1,31 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
-
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
-
-DROP DATABASE IF EXISTS cocktailsdb;
 CREATE DATABASE cocktailsdb;
+USE cocktailsdb;
+
+CREATE TABLE drinks
+(
+	id int NOT NULL AUTO_INCREMENT,
+	strDrink varchar(255) NOT NULL,
+    strCategory varchar(255) NOT NULL,
+	strDrinkThumb varchar(255) NOT NULL,
+    strIngredient1 varchar(255) NOT NULL,
+    strIngredient2 varchar(255) NOT NULL,
+    strIngredient3 varchar(255) NOT NULL,
+    strIngredient4 varchar(255) NOT NULL,
+    strIngredient5 varchar(255) NOT NULL,
+    strIngredient6 varchar(255) NOT NULL,
+    strIngredient7 varchar(255) NOT NULL,
+    strIngredient8 varchar(255) NOT NULL,
+    strIngredient9 varchar(255) NOT NULL,
+    strInstructions varchar(1000) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+SELECT * FROM cocktails_db.cocktails;
+
+SELECT 
+	strDrink
+    
+FROM drinks
+
+WHERE
+	strIngredient1 IN ('vodka')
