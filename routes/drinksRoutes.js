fe4c2b69004effa06b87cc2a3390386061a3928a
Melissa Knapp <melknapp@gmail.com>
2019-12-04 17:12:50 -0500

    Fixing travis ci errors

--- routes/drinksRoutes.js
+++ routes/drinksRoutes.js
@@ -1,31 +1,22 @@
 var db = require("../models");
 
-module.exports = function (app) {
-  app.post("/addDrink", function (req, res) {
+module.exports = function(app) {
+  app.post("/addDrink", function(req, res) {
     var newDrink = {
       strDrink: req.body.strDrink,
-      strIngredient1: req.body.strIngredient1,
-      strIngredient2: req.body.strIngredient2,
-      strIngredient3: req.body.strIngredient3,
-      strIngredient4: req.body.strIngredient4,
-      strIngredient5: req.body.strIngredient5,
-      strIngredient6: req.body.strIngredient6,
-      strIngredient7: req.body.strIngredient7,
-      strIngredient8: req.body.strIngredient8,
-      strIngredient9: req.body.strIngredient9,
-      strInstructions: req.body.strIntructions,
+      strIngredients: req.body.strIngredients,
       strDrinkThumb: req.body.strDrinkThumb
     };
-    db.create(newDrink).then(function (data) {
+    db.create(newDrink).then(function(data) {
       res.json(data);
     });
   });
-  app.get("/findDrinks", function (req, res) {
+  app.get("/findDrinks", function(req, res) {
     console.log(db);
     console.log(db.drink);
     db.drink
       .findAll({
-        where: db.sequelize.and(
+        where: db.sequelize.or(
           {
             strIngredients: db.sequelize.where(
               db.sequelize.fn("LOWER", db.sequelize.col("strIngredients")),
@@ -45,7 +36,7 @@ module.exports = function (app) {
           }
         )
       })
-      .then(function (dbDrinks) {
+      .then(function(dbDrinks) {
         res.json(dbDrinks);
         console.log("Length: " + dbDrinks.length);
       });
