cd2c6def8f973376913e7cceb8267943d5a47c90
Leah Kurtz <lfkurtz7@gmail.com>
2019-12-04 12:11:08 -0500

    schema

--- models/schema.sql
+++ models/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE drinks
 	PRIMARY KEY (id)
 );
 
-SELECT * FROM cocktails_db.cocktails;
+SELECT * FROM cocktailsdb.drinks;
 
 SELECT 
 	strDrink
@@ -28,4 +28,10 @@ SELECT
 FROM drinks
 
 WHERE
-	strIngredient1 IN ('vodka')
+	strIngredient1 IN ('vodka'); 
+
+SELECT strDrink
+FROM drinks
+WHERE strIngredient1||strIngredient2||strIngredient3||strIngredient4
+||strIngredient5||strIngredient6||strIngredient7||strIngredient8||strIngredient9 like '%vodka%'
+
