3b0220b065694d5df94f7be5664eddeab71d7fab
Melissa Knapp <melknapp@gmail.com>
2019-12-04 15:17:58 -0500

    search for rum now returns all drinks that contain any kind of rum

--- models/drinks.js
+++ models/drinks.js
@@ -4,48 +4,20 @@ module.exports = function(sequelize, Sequelize) {
       type: Sequelize.STRING,
       allowNull: false
     },
-    strIngredient1: {
+    strCategory: {
+      type: Sequelize.TEXT,
+      allowNull: true
+    },
+    strDrinkThumb: {
+      type: Sequelize.STRING
+    },
+    strIngredients: {
       type: Sequelize.TEXT,
       allowNull: false
-    },
-    strIngredient2: {
-      type: Sequelize.TEXT,
-      allowNull: false
-    },
-    strIngredient3: {
-      type: Sequelize.TEXT,
-      allowNull: true
-    },
-    strIngredient4: {
-      type: Sequelize.TEXT,
-      allowNull: true
-    },
-    strIngredient5: {
-      type: Sequelize.TEXT,
-      allowNull: true
-    },
-    strIngredient6: {
-      type: Sequelize.TEXT,
-      allowNull: true
-    },
-    strIngredient7: {
-      type: Sequelize.TEXT,
-      allowNull: true
-    },
-    strIngredient8: {
-      type: Sequelize.TEXT,
-      allowNull: true
-    },
-    strIngredient9: {
-      type: Sequelize.TEXT,
-      allowNull: true
     },
     strInstructions: {
       type: Sequelize.TEXT,
       allowNull: false
-    },
-    strDrinkThumb: {
-      type: Sequelize.STRING
     }
   });
   return Drink;
