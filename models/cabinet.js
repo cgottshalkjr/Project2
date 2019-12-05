e85087d51a03d7c57f352cc84d82cdda754ab733
Melissa Knapp <melknapp@gmail.com>
2019-12-05 12:50:42 -0500

    Retrieves all the ingredients in the user's cabinet from /api/findDrinks (hardcoded to userId=1 for now) and returns a list of all the ingredients in the database at /api/myIngredients

--- models/cabinet.js
+++ models/cabinet.js
@@ -1,12 +1,20 @@
-module.exports = function(sequelize, Sequelize) {
+module.exports = function (sequelize, Sequelize) {
   var Cabinet = sequelize.define("cabinet", {
     ingredients: {
       type: Sequelize.STRING,
       allowNull: false
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP()"),
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP()")
     }
   });
 
-  Cabinet.associate = function(models) {
+  Cabinet.associate = function (models) {
     Cabinet.belongsTo(models.user, {
       foreignKey: {
         allowNull: false
